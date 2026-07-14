c8707acd9386ce5554400f1c18a1d3e87d001a3f
unknown <maharanibanicaputri@gmail.com>
2026-07-14 09:47:19 +0700

    deploy

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,11 +1,12 @@
 <?php
 
-$host = "localhost";
+$host = "tokaido.proxy.rlwy.net";
 $user = "root";
-$pass = "";
-$db   = "perpustakaan";
+$pass = "gXnyGymUNJEtOFtgpphhKkpHjFphLMEP";
+$db   = "railway";
+$port = 37659;
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal : " . mysqli_connect_error());

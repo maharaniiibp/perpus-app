2253e7ba381ca2833e2ccea4478b7075d2aaebb6
unknown <maharanibanicaputri@gmail.com>
2026-07-14 10:03:12 +0700

    deploy docker

--- config/koneksi.php
+++ config/koneksi.php
@@ -9,7 +9,6 @@ $port = 37659;
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die("Koneksi gagal : " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
 ?>
